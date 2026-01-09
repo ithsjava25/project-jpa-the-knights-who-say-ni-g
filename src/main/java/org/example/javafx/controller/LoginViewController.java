fbf8ea384c3a34ce9a116e7533a9b4e4201dfab1
danne <xsz300@gmail.com>
2026-01-09 10:18:07 +0100

    Small preperation for loginview

--- src/main/java/org/example/javafx/controller/LoginViewController.java
+++ src/main/java/org/example/javafx/controller/LoginViewController.java
@@ -1,4 +1,16 @@
 package org.example.javafx.controller;
 
+import jakarta.inject.Inject;
+import org.example.service.CustomerService;
+
 public class LoginViewController {
+
+
+    @Inject
+    NavigationService navigator;
+    @Inject
+    CustomerService customerService;
+
+
+
 }
