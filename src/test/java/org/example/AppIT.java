b6eabeb248adf16ab0a8a3c7a1971523b94bda84
Ebba Andersson <eeebbaandersson@gmail.com>
2026-01-14 16:59:29 +0100

    Updated tests rentMovies/renewRentalMovies

--- src/test/java/org/example/AppIT.java
+++ src/test/java/org/example/AppIT.java
@@ -11,6 +11,7 @@ import org.example.service.RentalService;
 import org.example.tables.Customer;
 import org.example.tables.Movie;
 import org.example.tables.Rental;
+import org.example.tables.RentalMovie;
 import org.hibernate.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -21,6 +22,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -46,9 +48,7 @@ public class AppIT {
 
     @Test
     void verifyHibernateConnection() {
-        System.out.println("Försöker hämta SessionFactory");
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        System.out.println("SessionFactory hämtad!");
 
         try (Session session = sessionFactory.openSession()) {
             assertThat(session.isConnected()).isTrue();
@@ -57,7 +57,6 @@ public class AppIT {
             assertThat(result.toString()).isEqualTo("1");
         }
     }
-
 
     @Test
     void loginUser_shouldCreateNewUser_WhenEmailDoesNotExist() {
@@ -120,56 +119,106 @@ public class AppIT {
 
     }
 
-    // Todo: Se över efter ändrad projekt struktur
-//    @Test
-//    void createNewRental() {
-//        try (StatelessSession ss = HibernateUtil.getSessionFactory().openStatelessSession()) {
-//            Transaction tx = ss.beginTransaction();
-//            try {
-//                // Skapar test-kund och test-film och sparar
-//            Customer customer = new Customer("Förnamn", "Efternamn", "test@mail.com");
-//            ss.insert(customer);
-//
-//            Movie movie = new Movie("Interstellar", "Sci-Fi", new BigDecimal("99"));
-//            ss.insert(movie);
-//
-//                // Create rental-objekt
-//            LocalDateTime now = LocalDateTime.now();
-//            Rental rental = new Rental();
-//            rental.setCustomer(customer);
-//            rental.setRentalDate(now);
-//            rental.setReturnDate(now.plusHours(24));
-//            rental.setTotalRentalPrice(new BigDecimal("50.0"));
-//            // Sparar rental-object
-//            ss.insert(rental);
-//
-//            // Skapar kopplingstabellen manuellt
-//                // --> För att få in data i kopplingstabellen när vi använder Stateless Session
-//            ss.createNativeQuery(
-//                    "INSERT INTO movie_rental (rental_id, movie_id) VALUES (:rId, :mId)")
-//                .setParameter("rId", rental.getRentalId())
-//                .setParameter("mId", movie.getId())
-//                .executeUpdate();
-//
-//            tx.commit();
-//
-//            // Verifiering
-//            assertThat(rental.getRentalId()).isNotNull();
-//            // Kontrollerar kopplingen
-//            Object count = ss.createNativeQuery("SELECT COUNT(*) FROM movie_rental WHERE rental_id = :rId")
-//                .setParameter("rId", rental.getRentalId())
-//                .getSingleResult();
-//
-//            assertThat(((Number) count).intValue()).isEqualTo(1);
-//
-//        } catch (Exception e) {
-//                tx.rollback();
-//                throw e;
-//            }
-//        }
-//    }
 
-    // räkna total price
+    @Test
+    void rentMovies_ShouldCreateRentalAndRentalMoviesInDatabase() {
+
+        // Förbereder data
+        Customer customer = new Customer("Förnamn", "Efternamn", "test@mail.com");
+        Movie movie = new Movie("Interstellar", "Sci-Fi", new BigDecimal("99"));
+
+        try (StatelessSession ss = HibernateUtil.getSessionFactory().openStatelessSession()) {
+            Transaction tx = ss.beginTransaction();
+            ss.insert(customer);
+            ss.insert(movie);
+            tx.commit();
+        }
+
+        // Anropa RentalService
+        RentalService service = new RentalService();
+        service.rentMovies(customer, List.of(movie));
+
+        // Kontrollerar att datan hamnar i databasen
+        try (StatelessSession ss = HibernateUtil.getSessionFactory().openStatelessSession()) {
+            // Hämtar rental kopplat till kunden
+            String sql = "SELECT * FROM rental WHERE customer_id = :cId";
+            Rental savedRental = (Rental) ss.createNativeQuery(sql, Rental.class)
+                .setParameter("cId", customer.getCustomerId())
+                .getSingleResult();
+
+            assertThat(savedRental).isNotNull();
+
+            // Kontrollerar att RentalMovie skapades korrekt
+            String rmSql = "SELECT COUNT(*) FROM rental_movie WHERE rental_id = :rId";
+            Object count = ss.createNativeQuery(rmSql)
+                .setParameter("rId", savedRental.getRentalId())
+                .getSingleResult();
+
+            assertThat(((Number) count).intValue()).isEqualTo(1);
+
+        }
+    }
+
+
+    @Test
+    void renewRentalMovies_shouldIncreaseDateAndCost() {
+        Long rentalMovieID;
+        LocalDateTime currentDate = LocalDateTime.now();
+        BigDecimal initialAdditionalCost = new BigDecimal("0");
+
+        // Skapar data som ska uppdateras
+        try (StatelessSession ss = HibernateUtil.getSessionFactory().openStatelessSession()) {
+            Transaction tx = ss.beginTransaction();
+
+            // Skapar kund
+            Customer customer = new Customer("Förnamn", "Efternamn", "test@mail.com");
+            ss.insert(customer);
+
+            // Skapar film
+            Movie movie = new Movie("Interstellar", "Sci-Fi", new BigDecimal("99"));
+            ss.insert(movie);
+
+            // Kopplar kund till rental
+            Rental rental = new Rental();
+            rental.setCustomer(customer);
+            rental.setRentalDate(currentDate);
+            ss.insert(rental);
+
+            // Skapar RentalMovie
+            RentalMovie rm = new RentalMovie();
+            rm.setRental(rental);
+            rm.setMovie(movie);
+            rm.setPrice(new BigDecimal("99"));
+
+            rm.setRentedAt(currentDate);
+            rm.setAdditionalCost(initialAdditionalCost);
+
+            // Sparar och hämtar genererat ID
+            rentalMovieID = (Long) ss.insert(rm);
+            tx.commit();
+        }
+
+        // Anropar metoden via RentalService
+        RentalService service = new RentalService();
+        service.renewRentalMovie(rentalMovieID);
+
+        // Verifierar att uppdateringen skett korrekt
+        try (StatelessSession statelessSession =  HibernateUtil.getSessionFactory().openStatelessSession()) {
+            RentalMovie updatedRm = (RentalMovie) statelessSession.createNativeQuery(
+                "SELECT * FROM rental_movie WHERE movie_rental_id = :id",RentalMovie.class)
+                .setParameter("id", rentalMovieID)
+                .getSingleResult();
+
+            // Kontrollerar att kostnaden ökat med 29 kr från baseprice
+            assertThat(updatedRm.getAdditionalCost()).isEqualByComparingTo(new BigDecimal("29"));
+
+            //Kontrollera att datumet flyttats fram 24 timmar
+            assertThat(updatedRm.getRentedAt()).isEqualTo(currentDate.plusHours(24));
+
+        }
+    }
+
+   // Todo: Addera test för beräkna totalpris
 //    @Test
 //    void calculateTotalPriceForMovies(){
 //        // Skapa en instans av servicen manuellt för ett enhetstest
@@ -211,8 +260,6 @@ public class AppIT {
 
         }
     }
-
-    // Uppdatera filmuthyring med nytt pris och förlängd tid - U
 
 
     // Ta bort filmuthyrning, när 24h alt 48h passerat? - D
